bef7f4135508bfec200e7fe1fabd5d21a74e7ef5
Saga-5 <stalbekov.sultan05@gmail.com>
2025-03-03 03:18:10 +0600

    4fs home work

--- move_app/serializers.py
+++ move_app/serializers.py
@@ -3,33 +3,48 @@ from rest_framework import serializers
 
 
 class DirectorSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(min_length=2, max_length=100)
     movies_count = serializers.SerializerMethodField()
 
     class Meta:
         model = models.Director
-        fields = {"id", "name",'movies_count'}
+        fields = ["id", "name", "movies_count"]  # Используем список, а не множество
+
     def get_movies_count(self, director):
         return director.movies.count()
-
 
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Review
-        fields = ("id","text", "movie", "star")
+        fields = ("id", "text", "movie", "star")
+
+class ReviewValiditySerializer(serializers.ModelSerializer):
+    text = serializers.CharField()
+    star = serializers.IntegerField(min_value=1, max_value=5)
+    movie = serializers.IntegerField()
+
 
 class MovieSerializer(serializers.ModelSerializer):
     director = DirectorSerializer()
     reviews = ReviewSerializer(many=True, read_only=True)
     average_rating = serializers.SerializerMethodField()
+
     class Meta:
         model = models.Movie
-        fields = ("id", "title", "description", "duration", "director", 'reviews', "average_rating")
+        fields = ("id", "title", "description", "duration", "director", "reviews", "average_rating")
 
-        def get_average_rating(self, movie):
-            reviews = movie.reviews.all()
-            if reviews:
-                sum_reviews = sum(reviews.star for review in reviews)
-                average = sum_reviews / len(reviews)
-                return average
-            return None
+    def get_average_rating(self, movie):
+        reviews = movie.reviews.all()
+        if reviews:
+            sum_reviews = sum(review.star for review in reviews)  # Исправлено: правильно суммируем рейтинги
+            average = sum_reviews / len(reviews)
+            return average
+        return None
+
+
+class MovieValiditySerializer(serializers.Serializer):
+    title = serializers.CharField(min_length=2, max_length=100)
+    description = serializers.CharField()
+    duration = serializers.IntegerField()
+    director = serializers.IntegerField()
--- move_app/views.py
+++ move_app/views.py
@@ -1,6 +1,10 @@
+from wsgiref.validate import validator
+
 from django.shortcuts import render
 from . import models, serializers
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class DirectorlistAPIView(generics.ListAPIView):
@@ -17,17 +21,64 @@ class MovieListAPIView(generics.ListAPIView):
     queryset = models.Movie.objects.all()
     serializer_class = serializers.MovieSerializer
 
+    def post(self, request, *args, **kwargs):
+        validator = serializers.MovieValiditySerializer(data=request.data)
+        if not validator.is_valid():
+            return Response(validator.errors, status=status.HTTP_400_BAD_REQUEST)
+        title = validator.validated_data.get('title')
+        description = validator.validated_data.get('description')
+        director_id = validator.validated_data.get('director')
+        duration = validator.validated_data.get('duration')
+        movie = models.Movie.objects.create(title=title, description=description, director_id=director_id, duration=duration)
+        return Response(serializers.MovieSerializer(movie).data, status=status.HTTP_201_CREATED)
 
 class MovieDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Movie.objects.all()
     serializer_class = serializers.MovieSerializer
     lookup_field = 'id'
 
+    def put(self, request, *args, **kwargs):
+        movie_detail = self.get_object()
+        validator = serializers.MovieValiditySerializer(data=request.data)
+        if not validator.is_valid():
+            return Response(validator.errors, status=status.HTTP_400_BAD_REQUEST)
+        movie_detail.title = validator.validated_data.get('title')
+        movie_detail.description = validator.validated_data.get('description')
+        movie_detail.director_id = validator.validated_data.get('director')
+        movie_detail.duration = validator.validated_data.get('duration')
+        movie_detail.save()
+        return Response(serializers.MovieSerializer(movie_detail).data, status=status.HTTP_200_OK)
+
+
+
 class ReviewListAPIView(generics.ListAPIView):
     queryset = models.Review.objects.all()
     serializer_class = serializers.ReviewSerializer
+
+    def post(self, request, *args, **kwargs):
+        validator = serializers.ReviewValiditySerializer(data=request.data)
+        if not validator.is_valid():
+            return Response(validator.errors, status=status.HTTP_400_BAD_REQUEST)
+        text = validator.validated_data.get('text')
+        star = validator.validated_data.get('star',5)
+        movie_id = validator.validated_data.get('movie_id')
+        review = models.Review.objects.create(text=text, star=star, movie_id=movie_id)
+        review.save()
+        return Response(serializers.ReviewSerializer(review).data, status=status.HTTP_200_OK)
+
 
 class ReviewDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Review.objects.all()
     serializer_class = serializers.ReviewSerializer
     lookup_field = 'id'
+
+    def put(self, request, *args, **kwargs):
+        review_diteil = self.get_object()
+        validator = serializers.ReviewValiditySerializer(data=request.data)
+        if not validator.is_valid():
+            return Response(validator.errors, status=status.HTTP_400_BAD_REQUEST)
+        review_diteil.text = validator.validated_data.get('text')
+        review_diteil.star = validator.validated_data.get('star')
+        review_diteil.movie_id = validator.validated_data.get('movie_id')
+        review_diteil.save()
+        return Response(serializers.ReviewSerializer(review_diteil).data, status=status.HTTP_200_OK)
